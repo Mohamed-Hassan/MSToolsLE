ccc37fde0e54d10ad8697b8c601e4dffd145ad33
Mohamed Hassan <mah.q82@gmail.com>
2017-04-05 18:17:13 -0400

    Analysis tweets with crime dictionary and added flags

--- SMToolsLE.py
+++ SMToolsLE.py
@@ -21,11 +21,13 @@ def Response():
     if request.form['city'] == 'Erie':
         city = "42.129224,-80.085059,40km"
     elif request.form['city'] == 'Pittsburgh':
-        city = "40.440625,-79.995886,25Km"
+        city = "40.440625,-79.995886,35Km"
     elif request.form['city'] == 'Philadelphia':
         city = "39.9703447,-75.1377113,20km"
     elif request.form['city'] == 'Harrisburg':
         city = "40.273191,-76.886701,20km"
+    elif request.form['city'] == 'all':
+        city = " "
 
     dfrom = request.form['dfrom']
     dto = request.form['dTo']
@@ -38,62 +40,211 @@ def Response():
     if len(request.form) == 5 :
         wieght1 = request.form['wieght1']
         Keyword1 = request.form['Keyword1']
-        tweets = fetch_tweets(q=Keyword1, dateFrom= dfrom , dateTo= dto, city= city) # call fetch_tweets method and pass the form input
+        tweets = fetch_tweets(q=Keyword1, dateFrom= dfrom , dateTo= dto, city= city, wieght= wieght1 ) # call fetch_tweets method and pass the form input
         alltweets = tweets
     elif len(request.form) == 7 :
         wieght1 = request.form['wieght1']
         Keyword1 = request.form['Keyword1']
-        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
         wieght2 = request.form['wieght2']
         Keyword2 = request.form['Keyword2']
-        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
         alltweets = tweets + tweets2
     elif len(request.form) == 9 :
         wieght1 = request.form['wieght1']
         Keyword1 = request.form['Keyword1']
-        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
         wieght2 = request.form['wieght2']
         Keyword2 = request.form['Keyword2']
-        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
         wieght3 = request.form['wieght3']
         Keyword3 = request.form['Keyword3']
-        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets3 = fetch_tweets(q=Keyword3, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
         alltweets = tweets + tweets2 + tweets3
     elif len(request.form) == 11 :
         wieght1 = request.form['wieght1']
         Keyword1 = request.form['Keyword1']
-        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
         wieght2 = request.form['wieght2']
         Keyword2 = request.form['Keyword2']
-        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
         wieght3 = request.form['wieght3']
         Keyword3 = request.form['Keyword3']
-        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets3 = fetch_tweets(q=Keyword3, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
         wieght4 = request.form['wieght4']
         Keyword4 = request.form['Keyword4']
-        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
         alltweets = tweets + tweets2 + tweets3 + tweets4
     elif len(request.form) == 13 :
         wieght1 = request.form['wieght1']
         Keyword1 = request.form['Keyword1']
-        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
         wieght2 = request.form['wieght2']
         Keyword2 = request.form['Keyword2']
-        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
         wieght3 = request.form['wieght3']
         Keyword3 = request.form['Keyword3']
-        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets3 = fetch_tweets(q=Keyword3, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
         wieght4 = request.form['wieght4']
         Keyword4 = request.form['Keyword4']
-        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
         wieght5 = request.form['wieght5']
         Keyword5 = request.form['Keyword5']
-        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city)
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
         alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5
+    elif len(request.form) == 15 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword3, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5
+    elif len(request.form) == 17 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword3, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        wieght6 = request.form['wieght6']
+        Keyword6 = request.form['Keyword6']
+        tweets6 = fetch_tweets(q=Keyword6, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght6)
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5 + tweets6
+    elif len(request.form) == 19 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        wieght6 = request.form['wieght6']
+        Keyword6 = request.form['Keyword6']
+        tweets6 = fetch_tweets(q=Keyword6, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght6)
+        wieght7 = request.form['wieght7']
+        Keyword7 = request.form['Keyword7']
+        tweets7 = fetch_tweets(q=Keyword7, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght7)
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5 + tweets6 + tweets7
+    elif len(request.form) == 21 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        wieght6 = request.form['wieght6']
+        Keyword6 = request.form['Keyword6']
+        tweets6 = fetch_tweets(q=Keyword6, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght6)
+        wieght7 = request.form['wieght7']
+        Keyword7 = request.form['Keyword7']
+        tweets7 = fetch_tweets(q=Keyword7, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght7)
+        wieght8 = request.form['wieght8']
+        Keyword8 = request.form['Keyword8']
+        tweets8 = fetch_tweets(q=Keyword8, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght8)
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5 + tweets6 + tweets7 + tweets8
+    elif len(request.form) == 23 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        wieght6 = request.form['wieght6']
+        Keyword6 = request.form['Keyword6']
+        tweets6 = fetch_tweets(q=Keyword6, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght6)
+        wieght7 = request.form['wieght7']
+        Keyword7 = request.form['Keyword7']
+        tweets7 = fetch_tweets(q=Keyword7, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght7)
+        wieght8 = request.form['wieght8']
+        Keyword8 = request.form['Keyword8']
+        tweets8 = fetch_tweets(q=Keyword8, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght8)
+        wieght9 = request.form['wieght9']
+        Keyword9 = request.form['Keyword9']
+        tweets9 = fetch_tweets(q=Keyword9, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght9)
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5 + tweets6 + tweets7 + tweets8 + tweets9
+    elif len(request.form) == 25 :
+        wieght1 = request.form['wieght1']
+        Keyword1 = request.form['Keyword1']
+        tweets = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght1)
+        wieght2 = request.form['wieght2']
+        Keyword2 = request.form['Keyword2']
+        tweets2 = fetch_tweets(q=Keyword2, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght2)
+        wieght3 = request.form['wieght3']
+        Keyword3 = request.form['Keyword3']
+        tweets3 = fetch_tweets(q=Keyword1, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght3)
+        wieght4 = request.form['wieght4']
+        Keyword4 = request.form['Keyword4']
+        tweets4 = fetch_tweets(q=Keyword4, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght4)
+        wieght5 = request.form['wieght5']
+        Keyword5 = request.form['Keyword5']
+        tweets5 = fetch_tweets(q=Keyword5, dateFrom=dfrom, dateTo=dto, city= city, wieght= wieght5)
+        wieght6 = request.form['wieght6']
+        Keyword6 = request.form['Keyword6']
+        tweets6 = fetch_tweets(q=Keyword6, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght6)
+        wieght7 = request.form['wieght7']
+        Keyword7 = request.form['Keyword7']
+        tweets7 = fetch_tweets(q=Keyword7, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght7)
+        wieght8 = request.form['wieght8']
+        Keyword8 = request.form['Keyword8']
+        tweets8 = fetch_tweets(q=Keyword8, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght8)
+        wieght9 = request.form['wieght9']
+        Keyword9 = request.form['Keyword9']
+        tweets9 = fetch_tweets(q=Keyword9, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght9)
+        wieght10 = request.form['wieght10']
+        Keyword10 = request.form['Keyword10']
+        tweets10 = fetch_tweets(q=Keyword10, dateFrom=dfrom, dateTo=dto, city=city, wieght=wieght10)
+
+        alltweets = tweets + tweets2 + tweets3 + tweets4 + tweets5 + tweets6 + tweets7 + tweets8 + tweets9 + tweets10
+
 
 
     print  alltweets
-    fd = request.form
+    fd = request.form.to_dict()
 
     return render_template("UI.html", Response = alltweets, formData = fd )  # render the list tweet to UI.html
 
--- fechTweepy.py
+++ fechTweepy.py
@@ -1,11 +1,19 @@
+import re
+import string
 
-import  CONFIG
+import CDictionary
+import CONFIG
 import tweepy
-import json
+from operator import itemgetter
+
+def cleanTweet(text):
+    text = re.sub(r"http\s+", "",text)
+    tbl = string.maketrans('','')
+    text.translate(tbl,string.punctuation)
+    return text.split(' ')
 
 
-
-def fetch_tweets(q,dateFrom,dateTo,city):
+def fetch_tweets(q,dateFrom,dateTo,city,wieght):
     consumer_key = CONFIG.PASS['consumer_key']
     consumer_secret = CONFIG.PASS['consumer_secret']
     access_token = CONFIG.PASS['access_token']
@@ -16,16 +24,31 @@ def fetch_tweets(q,dateFrom,dateTo,city):
 
     api = tweepy.API(auth)
     print api
+    wieght = wieght.encode('utf-8')
+    wieght = int(wieght)
     max_tweets = 50    # eliminate the number of tweets to control the process as a draft
     #searched_tweets = [status.text.encode('utf-8') for status in tweepy.Cursor(api.search, q=q).items(max_tweets)]   "5.29126,52.132633,250km"
     searched_tweets = [status for status in tweepy.Cursor(api.search,q=q,since= dateFrom ,until= dateTo,geocode= city).items(max_tweets)]
 
     tweets_data = []
     for line in searched_tweets:
-        alltweetvalues = [str(line.user.screen_name.encode('utf-8')), str(line.created_at),str(line.text.encode('utf-8'))]
-        tweets_data.append(alltweetvalues)    # encoding text data
+        atweet = [str(line.user.screen_name.encode('utf-8')), str(line.created_at),str(line.text.encode('utf-8'))]
 
-    return tweets_data
+        words = line.text.encode('utf-8') #extracting only text for analysis
+        words = cleanTweet(words)
+        count = 0
+        for w in words:
+            if w in CDictionary.crimeDict:
+                count += 1
+        scale = count + wieght
+        atweet.insert(0,scale)
+        tweets_data.append(atweet)    # encoding text data
+
+
+    sorTweets = sorted(tweets_data, key=itemgetter(0), reverse=True)
+    print '========================='
+    print sorTweets
+    return sorTweets
 
 
 
